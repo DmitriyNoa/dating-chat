bf7855fc684c01cb30383417f896ec59ac84e496
dzharkov <dmytro.zharkov@zalando.de>
2017-08-13 17:06:07 +0200

    Deploy setup

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.use(expressValidator());
 app.use(lusca.xframe(APP_CONSTANTS.SAMEORIGIN));
 app.use(lusca.xssProtection(true));
 
-app.use(express.static(path.join(__dirname, "public"), {maxAge: 31557600000}));
+//app.use(express.static(path.join(__dirname, "public"), {maxAge: 31557600000}));
 
 app.get("/", homeController.index);
 
